9c7152d176122e61134542d088de1193bdb3791f
Siegfred Samson <siegfredsamson@gmail.com>
2021-06-20 00:54:12 +0100

    signup created in designer.views

--- belleda/designer/views.py
+++ belleda/designer/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Designer
 
 
 def become_designer(request):
+    # check if the form has been submitted
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            designer = Designer.objects.create(
+                name=user.username, created_by=user)
+
+            return redirect('base')
+
     return render(request, 'become_designer.html')
