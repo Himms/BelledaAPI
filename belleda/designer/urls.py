c2d2193314bd63671d0e4b64c15184212ba642c5
Siegfred Samson <siegfredsamson@gmail.com>
2021-06-20 12:08:40 +0100

    designers urls.py modified for logout

--- belleda/designer/urls.py
+++ belleda/designer/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('become-designer/', views.become_designer, name='become_designer'),
-    path('designer-admin/', views.designer_admin, name='designer_admin')
+    path('designer-admin/', views.designer_admin, name='designer_admin'),
+    path('logout/', auth_views.LogoutView.as_view, name='logout'),
 ]
