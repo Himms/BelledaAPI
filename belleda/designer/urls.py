7588301dc47394b2192f04cdb98975b5a675e11c
Siegfred Samson <siegfredsamson@gmail.com>
2021-06-20 11:08:46 +0100

    login changes registered in designers urls.py

--- belleda/designer/urls.py
+++ belleda/designer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('become-designer/', views.become_designer, name='become_designer')
+    path('become-designer/', views.become_designer, name='become_designer'),
+    path('designer-admin/', views.designer_admin, name='designer_admn')
 ]
