b42bae83cc6d0c5eb2f80a1685fe36058b664c4e
Siegfred Samson <siegfredsamson@gmail.com>
2021-06-25 13:39:28 +0100

    view.nav_item path added in belleda_main.urls.py

--- belleda/belleda_main/urls.py
+++ belleda/belleda_main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
+    path('', views.frontpage, name='frontpage'),
+    path('nav_item/', views.nav_item, name='nav_item'),
 ]
