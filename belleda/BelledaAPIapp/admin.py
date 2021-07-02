ebc99c97bbdbf20e8fa17c1d021b903ad3415cd6
Siegfred Samson <siegfredsamson@gmail.com>
2021-07-02 23:05:53 +0100

    Cartegory, Design and Cart, registered in admin.py

---
+++ belleda/BelledaAPIapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Category, Design, Cart
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Design)
+admin.site.register(Cart)
